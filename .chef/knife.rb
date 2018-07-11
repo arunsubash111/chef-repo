b71e3df007bbbfd5885ecc4bd96e2a36288687f9
Arun Subash <arunsubash111@hotmail.com>
2018-07-11 17:34:00 +0530

    some commit

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,8 @@ node_name                "asubash"
 client_key               "#{current_dir}/asubash.pem"
 chef_server_url          "https://api.chef.io/organizations/sandbox123"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+
+
+
+
+
